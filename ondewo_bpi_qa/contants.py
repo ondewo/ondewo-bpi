29f460bea6591924dde53aceee5eead676255579
Gabriel Rodrigues <gabriel.amarista.rodrigues@ondewo.com>
2022-04-21 17:29:57 +0200

    Add helper and constants

---
+++ ondewo_bpi_qa/contants.py
@@ -0,0 +1,7 @@
+QA_URL_FILTER_CONTEXT_NAME: str = "c-qa-url-filter"
+QA_URL_FILTER_DEFAULT_PARAM_NAME: str = 'default-filter'
+QA_URL_FILTER_PROVISIONAL_PARAM_NAME: str = 'provisional-filter'
+QA_URL_DEFAULT_FILTER: str = '.*'
+
+CAI_RESPONSE_NAME: str = 'cai_response'
+QA_RESPONSE_NAME: str = 'qa_response'
