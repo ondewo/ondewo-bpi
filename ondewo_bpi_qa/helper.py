5e28fc221c5a4c7cf7cf45955cf7ca9074decaeb
Gabriel Rodrigues <gabriel.amarista.rodrigues@ondewo.com>
2022-04-27 11:48:20 +0200

    Add helpers to extract uuids from paths

--- ondewo_bpi_qa/helper.py
+++ ondewo_bpi_qa/helper.py
@@ -23,6 +23,22 @@ class ContextHelper:
         agent_path: str = match.group()
         return agent_path
 
+    @classmethod
+    def get_last_uuid_from_path(cls, path: str) -> str:
+        """ Extract the last resource uuid from the full path.
+
+        Examples:
+            "projects/project_1/agent/sessions/123" -> "123"
+            "projects/project_1/agent/sessions/123/reviews/456" -> "456"
+
+        Args:
+            path: full DFv2 name in format "projects/<project_id>/agent/.../<uuid>"
+
+        Returns:
+            last resource uuid of the path
+        """
+        return path.rsplit('/', maxsplit=1)[-1]
+
 
 if __name__ == '__main__':
     print(f'should match: {ContextHelper.get_agent_path_from_path("projects/9c4e97ab-13ec-4b4b-bade-256b5c6e1bb2/agent/sessions/9c4e97ab-13ec-4b4b-bade-256b5c6e1bb2")}')
